c21cc71fcbc336e1243cc721ae3b589fa2b84832
EmmanuelR0411 <emmanuelreyna0410@gmail.com>
2022-03-11 16:28:27 -0600

    Creacion aplicacion para buscar gifs

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/service/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent implements OnInit {
+
+  public busquedas: string[] = [];
+
+  constructor(private gifsService: GifsService) { }
+
+  ngOnInit(): void {
+  }
+
+  get historial() {
+    return this.gifsService.historial;
+  }
+
+  buscar( item: string ){
+    this.gifsService.buscarGifs(item);
+  }
+
+}
